32d03baff887f37dd7a5cb1af8b4a59ea396e6d7
hughkhan <hughkhan@yahoo.com>
2025-01-26 14:03:50 -0600

    Add new OpenAI API Key

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
-OPENAI_API_KEY = "sk-proj-zJ5BH1s5cz7b92Su4SHdT3BlbkFJNMpNA1yb9Jx35a1rjClv"     #Directly through OpenAI
-#OPENAI_API_KEY ="4c217676012e44e8958e8984aa839903"      #Through https://aimlapi.com/.  Access to various other llm providers  
+#OPENAI_API_KEY = "sk-proj-zJ5BH1s5cz7b92Su4SHdT3BlbkFJNMpNA1yb9Jx35a1rjClv"
+OPENAI_API_KEY = "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"     #Directly through OpenAI
 HUGGINGFACEHUB_API_TOKEN="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"
 WOLFRAM_ALPHA_APPID = "5VQ5RQ-GG2PK3XYTA"
 LANGCHAIN_TRACING_V2 = "true"
 LANGCHAIN_API_KEY = "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"
-AIMLAPI_KEY = "4c217676012e44e8958e8984aa839903"
+AIMLAPI_KEY = "4c217676012e44e8958e8984aa839903"        #Through https://aimlapi.com/.  Access to various other llm providers 
 VENDOR_URL = "https://api.aimlapi.com/v1/"
 
 
--- naics_llm_direct.py
+++ naics_llm_direct.py
@@ -3,6 +3,7 @@ import os
 
 def get_naics_llm_direct():
 
+    #llm = ChatOpenAI(model="gpt-4o", temperature=0)
     llm = ChatOpenAI(                       #Langchain OpenAI Model Wrapper but going through AIML.com
         openai_api_base=os.environ["VENDOR_URL"],
         openai_api_key=os.environ["AIMLAPI_KEY"],
@@ -10,8 +11,9 @@ def get_naics_llm_direct():
         #model_name="meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo")
         #model_name="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo")
         #model_name="google/gemma-7b-it")
-        model_name="claude-3-sonnet-20240229")
-    
+        #model_name="claude-3-sonnet-20240229")
+        model_name="gpt-4o")
+   
     query = input("Please enter your primary business activity: ")
     
     #resultfromllm = llm.invoke("input": f"Which of the following numbered NAICS classification titles best fits the business which developers software products:\n{}"})
